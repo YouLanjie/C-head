e75ef433ee79f75e5db8e9c09cdb09a9d5d96d88
youlanjie <ylj18926385312@163.com>
2021-06-01 15:44:33 +0800

    增加pid.h文件

---
+++ pid/pid.h
@@ -0,0 +1,16 @@
+/*File name: pid.h*/
+/*This file use to include the head file*/
+
+#include <stdio.h>
+#include <stdlib.h>    //exit();
+#include <string.h>
+
+/*About Linux process head file*/
+/*Use fork() to make process*/
+/*Use kill() to kill process*/
+#include <sys/types.h> //pid_t
+#include <signal.h>    //signal();
+#include <unistd.h>    //pause();
+#include <wait.h>      //wait();
+
+
