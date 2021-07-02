0f20e9025ee8953dd178a2146d4a55751a88d8f9
youlanjie <ylj18926385312@163.com>
2021-07-02 21:55:00 +0800

    删除MENU宏定义

--- h-file/include.h
+++ h-file/include.h
@@ -1,6 +1,9 @@
 /* File name: include.h */
 /* This is a head file */
 
+#ifndef _INCLUDE_H_
+#define _INCLUDE_H_
+
 /* include head file */
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,8 +12,17 @@
 /* pause() */
 #include <sys/stat.h>
 /* pass */
+#include <sys/types.h>
+/* pass */
 #include <string.h>
 /* strcat() strcmp() strcpy() */
+
+#ifndef Clear
+	#define Clear printf("\033[2J\033[1;1H");
+#endif
+#ifndef Clear2
+	#define Clear2 system("clear");
+#endif
 
 /* kbhit */
 int kbhit();
@@ -19,19 +31,15 @@ int kbhit_if();
 int kbhit2();
 
 /* menu */
-#ifndef Menu
-	#define Menu printf("\033[0m\033[11;11H");
-#endif
-#ifndef Menu2
-	#define Menu2 printf("\033[0m\033[11;19H");
-#endif
-void menu(char a[50]);
-void menu2(char a[50]);
+void menu(char title[50], short p, short a);
+void menu2(char title[50]);
 
 /* pid */
-#include <sys/types.h>
+/* #include <sys/types.h> */
 /* pid_t */
 #include <signal.h>
 /* signal() */
 #include <wait.h>
+
+#endif
 
