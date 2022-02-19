aa44857c998ae19feee7208eb55fccd622ed6c13
youlanjie <ylj18926385312@163.com>
2022-02-19 21:48:28 +0800

    添加大量的预定义以移植到windows

--- tool/tool/kbhit.c
+++ tool/tool/kbhit.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
-#include <termios.h>
-#include <unistd.h>
-#include <fcntl.h>
+#ifdef __linux
+	#include <stdio.h>
+	#include <termios.h>
+	#include <unistd.h>
+	#include <fcntl.h>
 
-/* 循环输入 */
-int Kbhit() {
-	struct termios oldt, newt;
-	int ch;
-	int oldf;
-	do {
+	/* 判断有没有输入 */
+	extern int kbhit() {
+		struct termios oldt, newt;
+		int ch;
+		int oldf;
 		tcgetattr(STDIN_FILENO, &oldt);
 		newt = oldt;
 		newt.c_lflag &= ~(ICANON | ECHO);
@@ -20,66 +20,60 @@ int Kbhit() {
 		fcntl(STDIN_FILENO, F_SETFL, oldf);
 		if(ch != EOF) {
 			ungetc(ch, stdin);
-			return ch;                      //有键入字符返回键入字符
+			return 1;
 		}
-	} while (ch == EOF);                            //如果没有键入字符则重来一次
-	return 0;
-}
-
-/* 去除kbhit后遗症 */
-int Input() {
-	int a;
-	a = Kbhit();
-	getchar();
-	return a;
-}
-
-/* 判断有没有输入 */
-int KbhitHas() {
-	struct termios oldt, newt;
-	int ch;
-	int oldf;
-	tcgetattr(STDIN_FILENO, &oldt);
-	newt = oldt;
-	newt.c_lflag &= ~(ICANON | ECHO);
-	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-	oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-	fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-	ch = getchar();
-	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-	fcntl(STDIN_FILENO, F_SETFL, oldf);
-	if(ch != EOF) {
-		ungetc(ch, stdin);
-		return 1;
+		return 0;
 	}
-	return 0;
-}
 
-/* 不循环输入 */
-int KbhitNoTime() {
-	struct termios oldt, newt;
-	int ch;
-	int oldf;
-	tcgetattr(STDIN_FILENO, &oldt);
-	newt = oldt;
-	newt.c_lflag &= ~(ICANON | ECHO);
-	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-	oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-	fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-	ch = getchar();
-	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-	fcntl(STDIN_FILENO, F_SETFL, oldf);
-	if(ch != EOF) {
-		ungetc(ch, stdin);
+	/* 不循环输入 */
+	extern int kbhitGetchar() {
+		struct termios oldt, newt;
+		int ch;
+		int oldf;
+		tcgetattr(STDIN_FILENO, &oldt);
+		newt = oldt;
+		newt.c_lflag &= ~(ICANON | ECHO);
+		tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+		oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+		fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+		ch = getchar();
+		tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+		fcntl(STDIN_FILENO, F_SETFL, oldf);
+		if(ch != EOF) {
+			ungetc(ch, stdin);
+			return ch;
+		}
+		return 0;
+	}
+
+	extern int getch() {
+		struct termios tm, tm_old;
+		int fd = 0, ch;
+	 
+		if (tcgetattr(fd, &tm) < 0) {                   //保存现在的终端设置
+			return -1;
+		}
+		tm_old = tm;
+		cfmakeraw(&tm);                                 //更改终端设置为原始模式，该模式下所有的输入数据以字节为单位被处理
+		if (tcsetattr(fd, TCSANOW, &tm) < 0) {          //设置上更改之后的设置
+			return -1;
+		}
+		ch = getchar();
+		if (tcsetattr(fd, TCSANOW, &tm_old) < 0) {      //更改设置为最初的样子
+			return -1;
+		}
 		return ch;
 	}
-	return 0;
-}
-
-/*
-这个kbhit函数用于input输入
-在调用的时候将返回值赋值给一个int变量
-可用那个变量判断
-输入字符时毫无延迟
-*/
+#endif
+#ifdef _WIN32
+	#include <conio.h>
+	int kbhitGetchar() {
+		if (kbhit()) {
+			return getch();
+		}
+		else {
+			return 0;
+		}
+	}
+#endif
 
--- tool/tool/menu.c
+++ tool/tool/menu.c
@@ -1,44 +1,89 @@
-#include <asm-generic/ioctls.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/ioctl.h>
 #include <unistd.h>
+
+#ifdef __linux
+	#include <sys/ioctl.h>
+	#include <asm-generic/ioctls.h>
+	#ifndef fontColorSet
+		#define fontColorSet(a,b) printf("\033[%d;%dm",a, b)
+	#endif
+	#ifndef gotoxy
+		#define gotoxy(x,y) printf("\033[%d;%dH",x, y)
+	#endif
+#endif
+#ifdef _WIN32
+	#include <windows.h>
+	void gotoxy(int x,int y)//光标定位函数
+	{
+		COORD p;//定义结构体变量p
+		HANDLE handle=GetStdHandle(STD_OUTPUT_HANDLE);//获取当前函数句柄
+		p.X=x;p.Y=y;//将光标的目标移动位置传递给结构体
+		SetConsoleCursorPosition(handle,p);//移动光标
+	}
+#endif
 
 /* 菜单#1 */
 void Menu(char title[], short pages, short allPages) {
-	struct winsize size;
+	#ifdef __linux
+		struct winsize size;
+		ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
+		int winSizeCol = size.ws_col;
 
-	ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
-	printf("\033[2;32m\033[6;%dH↑\033[10;%dH↓\033[11;%dH\033[2;32m%d/%d\033[1;33m", size.ws_col / 2 - 1, size.ws_col / 2 - 1, size.ws_col / 2 + 25, pages,allPages);
-	printf("\033[2;%dH\033[1;32m%s", size.ws_col / 2 - (int)strlen(title) / 2, title);
-	printf("\033[5;%dH\033[34m--------------------------------------------------------", size.ws_col / 2 - 27);
-	printf("\033[6;%dH\033[34m|\033[54C|", size.ws_col / 2 - 27);
-	printf("\033[7;%dH|\033[54C|", size.ws_col / 2 - 27);
-	printf("\033[8;%dH|\033[54C|", size.ws_col / 2 - 27);
-	printf("\033[9;%dH|\033[54C|", size.ws_col / 2 - 27);
-	printf("\033[10;%dH|\033[54C|", size.ws_col / 2 - 27);
-	printf("\033[11;%dH|\033[54C|", size.ws_col / 2 - 27);
-	printf("\033[12;%dH|\033[54C|", size.ws_col / 2 - 27);
-	printf("\033[13;%dH--------------------------------------------------------\033[0m\033[11;11H", size.ws_col / 2 - 27);
-	printf("\033[11;%dH\033[1;31m请选择:\033[0m\033[11;11H", size.ws_col / 2 - 23);
+		printf("\033[2;32m\033[6;%dH↑\033[10;%dH↓\033[11;%dH\033[2;32m%d/%d\033[1;33m", winSizeCol / 2 - 1, winSizeCol / 2 - 1, winSizeCol / 2 + 25, pages,allPages);
+		printf("\033[2;%dH\033[1;32m%s", winSizeCol / 2 - (int)strlen(title) / 2, title);
+		printf("\033[5;%dH\033[34m--------------------------------------------------------\033[34m", winSizeCol / 2 - 27);
+		for (int i = 0; i < 6; i++) {
+			gotoxy(i + 6, winSizeCol / 2 - 27);
+			printf("|\033[54C|");
+		}
+		printf("\033[13;%dH--------------------------------------------------------\033[0m", winSizeCol / 2 - 27);
+		printf("\033[11;%dH\033[1;31m请选择:\033[0m", winSizeCol / 2 - 23);
+	#endif
+	#ifdef _WIN32
+		int winSizeCol = 56;
+
+		gotoxy(6, winSizeCol / 2 - 1); printf("↑"); gotoxy(10, winSizeCol / 2 - 1); printf("↓"); gotoxy(11, winSizeCol / 2 + 25); printf("%d/%d\033[1;33m", pages,allPages);
+		gotoxy(2, winSizeCol / 2 - (int)strlen(title) / 2); printf("%s", title);
+		gotoxy(5, winSizeCol / 2 - 27); printf("\033[34m--------------------------------------------------------");
+		for (int i = 0; i < 6; i++) {
+			gotoxy(i + 6, winSizeCol / 2 - 27);
+			printf("|\033[54C|");
+		}
+		gotoxy(13, winSizeCol / 2 - 27); printf("--------------------------------------------------------");
+		gotoxy(11, winSizeCol / 2 - 23); printf("请选择:");
+	#endif
 	return;
 }
 
 /* 菜单#2 */
 void Menu2(char title[]) {
-	struct winsize size;
+	#ifdef __linux
+		struct winsize size;
+		ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
+		int winSizeCol = size.ws_col;
 
-	ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
-	printf("\033[2;%dH\033[1;32m%s", size.ws_col / 2 - (int)strlen(title) / 2, title);
-	printf("\033[5;%dH\033[34m--------------------------------------------------------", size.ws_col / 2 - 27);
-	printf("\033[6;%dH\033[34m|\033[54C|", size.ws_col / 2 - 27);
-	printf("\033[7;%dH|\033[54C|", size.ws_col / 2 - 27);
-	printf("\033[8;%dH|\033[54C|", size.ws_col / 2 - 27);
-	printf("\033[9;%dH|\033[54C|", size.ws_col / 2 - 27);
-	printf("\033[10;%dH|\033[54C|", size.ws_col / 2 - 27);
-	printf("\033[11;%dH|\033[54C|", size.ws_col / 2 - 27);
-	printf("\033[12;%dH|\033[54C|", size.ws_col / 2 - 27);
-	printf("\033[13;%dH--------------------------------------------------------\033[0m\033[11;11H", size.ws_col / 2 - 27);
-	printf("\033[11;%dH\033[1;31m按任意按键返回：\033[0m\033[11;19H", size.ws_col / 2 - 23);
+		printf("\033[2;%dH\033[1;32m%s", winSizeCol / 2 - (int)strlen(title) / 2, title);
+		printf("\033[5;%dH\033[34m--------------------------------------------------------", winSizeCol / 2 - 27);
+		for (int i = 0; i < 7; i++) {
+			gotoxy(i + 6, winSizeCol / 2 - 27);
+			printf("|\033[54C|");
+		}
+		printf("\033[13;%dH--------------------------------------------------------\033[0m", winSizeCol / 2 - 27);
+		printf("\033[11;%dH\033[1;31m按任意按键返回：\033[0m", winSizeCol / 2 - 23);
+	#endif
+	#ifdef _WIN32
+		int winSizeCol = 56;
+
+		gotoxy(2, winSizeCol / 2 - (int)strlen(title) / 2); printf("%s", title);
+		gotoxy(5, winSizeCol / 2 - 27); printf("\033[34m--------------------------------------------------------");
+		for (int i = 0; i < 6; i++) {
+			gotoxy(i + 6, winSizeCol / 2 - 27);
+			printf("|\033[54C|");
+		}
+		gotoxy(13, winSizeCol / 2 - 27); printf("--------------------------------------------------------");
+		gotoxy(11, winSizeCol / 2 - 23); printf("按任意按键返回：");
+	#endif
 	return;
 }
+
